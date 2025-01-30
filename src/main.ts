c59a43bbf9bff5fe3a2bc9be79b852a2b03aaf1c
merisir573 <aikosstory@gmail.com>
2025-01-30 13:52:25 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { HttpModule } from '@nestjs/axios';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://finals-gateway.onrender.com', // Gateway's domain
+    methods: 'GET, POST, PUT, DELETE', // Customize allowed HTTP methods
+    allowedHeaders: 'Content-Type, Authorization', // Customize allowed headers
+  });
   await app.listen(3000);
 }
 bootstrap();
