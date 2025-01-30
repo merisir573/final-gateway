4294400db48a11de55b60f2a2bfe714f3346f6c3
merisir573 <aikosstory@gmail.com>
2025-01-30 16:05:24 +0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GatewayController } from './gateway/gateway.controller';
 import { GatewayService } from './gateway/gateway.service';
-import { HttpModule, HttpService } from '@nestjs/axios';
+import { HttpModule} from '@nestjs/axios';
 
 @Module({
   imports: [HttpModule],
   controllers: [AppController, GatewayController],
-  providers: [AppService, GatewayService, HttpService],
+  providers: [AppService, GatewayService],
 })
 export class AppModule {}
